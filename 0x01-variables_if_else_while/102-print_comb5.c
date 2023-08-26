cb1470ec17599877ad9b451edc8252579face17c
Jamo <jameskouchade01@gmail.com>
2023-08-26 14:12:42 -0700

    Combination 4.0

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i,j,k,l;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for ( k = '0'; k <= '9'; k++)
+			{
+				for (l = '1'; l <= '9')
+				{
+					if (i + j < k + l)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+					}
+				}
+				if (i != 9 || j != 8 || k != 9 || l != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
