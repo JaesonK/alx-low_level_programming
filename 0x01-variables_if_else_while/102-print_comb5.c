ec050e5fee92b1e22729c94257551c138fb4195a
Jamo <jameskouchade01@gmail.com>
2023-08-26 15:11:42 -0700

    Combination 4.0git

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-        int i, j, k, l;
-
-        for (i = '0'; i <= '9'; i++)
-        {
-                for (j = '0'; j <= '9'; j++)
+	int i, j, k, l;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
                 {
                         for (k = '0'; k <= '8'; k++)
                         {
