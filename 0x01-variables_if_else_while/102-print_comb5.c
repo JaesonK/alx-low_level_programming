d8e2008c66880e2825d7c073c64f571d92c5dfde
Jamo <jameskouchade01@gmail.com>
2023-08-26 15:08:39 -0700

    Combination 4.0git

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,36 +10,36 @@
 
 int main(void)
 {
-	int i, j, k, l;
+        int i, j, k, l;
 
-	for (i = '0'; i <= '9'; i++)
-	{
-		for (j = '0'; j <= '9'; j++)
-		{
-			for (k = '0'; k <= '8'; k++)
-			{
-				for (l = '0'; l <= '9'; l++)
-				{
-					if (i == 0 && j == 0 && k == 0 && l == 0)
-						l = '1';
-					if (i + j < k + l)
-					{
-						putchar(i);
-						putchar(j);
-						putchar(' ');
-						putchar(k);
-						putchar(l);
-						if (i != 9 || j != 8 || k != 9 || l != 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
+        for (i = '0'; i <= '9'; i++)
+        {
+                for (j = '0'; j <= '9'; j++)
+                {
+                        for (k = '0'; k <= '8'; k++)
+                        {
+                                for (l = '0'; l <= '9'; l++)
+                                {
+                                        if (i == 0 && j == 0 && k == 0 && l == 0)
+                                                l = '1';
+                                        if (i + j < k + l)
+                                        {
+                                                putchar(i);
+                                                putchar(j);
+                                                putchar(' ');
+                                                putchar(k);
+                                                putchar(l);
+                                                if (i != 9 || j != 8 || k != 9 || l != 9)
+                                                {
+                                                        putchar(',');
+                                                        putchar(' ');
+                                                }
+                                        }
+                                }
+                        }
+                }
+        }
+        putchar('\n');
 
-	return (0);
+        return (0);
 }
