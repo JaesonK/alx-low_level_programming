f29bce56eb6931cba8ebe66c498f4f85d2c3299a
Jamo <jameskouchade01@gmail.com>
2023-08-26 08:05:28 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * return: 0 Alaways (Success)
+ */
+
+int main(void)
+{
+	int i;
+	
+	for (i = '0', i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(", ");
+	}
+
+	return (0);
+}
+
