be6c7ebd05818f02dda89d921a0182e2d67afaea
Jamo <jameskouchade01@gmail.com>
2023-08-26 07:52:17 -0700

    Base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,12 @@
  * main - Entry point
  *
  * Return: 0 Always (Success)
- * */
+ */
 
-int main (void)
+int main(void)
 {
 	int i;
-	
+
 	for (i = '0'; i <= '9'; i++)
 		putchar(i);
 	for (i = 'a'; i <= 'f'; i++)
