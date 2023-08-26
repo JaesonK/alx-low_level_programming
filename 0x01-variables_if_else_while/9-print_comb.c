9e1f00f8dd1afb3b81c101f41e8b1f0ddee42986
Jamo <jameskouchade01@gmail.com>
2023-08-26 08:14:19 -0700

    Combination digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,21 +5,21 @@
 /**
  * main - Entry point
  *
- * return: 0 Alaways (Success)
+ * return: Alaways 0 (Success)
  */
 
 int main(void)
 {
 	int i;
-	
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 		if (i != '9')
 		{
 			putchar(',');
-			putchar(' ');
 		}
+		putchar(' ');
 	}
 
 	return (0);
