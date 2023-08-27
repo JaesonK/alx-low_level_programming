25725232e55c4006fe4cb39ad0dd71259fe76a09
Jamo <jameskouchade01@gmail.com>
2023-08-27 11:04:28 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char word[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		putchar(word[i]);
+	putchar('\n');
+
+	return (0);
+}
