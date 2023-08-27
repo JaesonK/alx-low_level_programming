c60927d6a3453dfafd8f741a254b63b2568eb7ff
Jamo <jameskouchade01@gmail.com>
2023-08-27 14:59:55 -0700

    ASCII

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check Holberton
+ * @c: An input character
+ * Description: function returns 1 if the character is a
+ * letter, lowercase or uppercase.
+ * Return: 1 or 0 in otherwise.
+ */
+int _isalpha(int c)
+{
+	int ac = (int)c;
+
+	if ((ac >= 65 && ac <= 90) || (ac >= 97 && ac <= 122))
+		return (1);
+	else
+		return (0);
+}
