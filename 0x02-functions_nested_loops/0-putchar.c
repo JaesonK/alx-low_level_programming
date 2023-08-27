aca04f19bd60c12047133c67af93327af99ed66e
Jamo <jameskouchade01@gmail.com>
2023-08-27 12:44:33 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,12 @@
  *Description: It prints the word Holberton, followed by a new line.
  * Return: 0.
  */
-
 int main(void)
 {
 	char word[9] = "_putchar";
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 8; i++)
 		putchar(word[i]);
 	putchar('\n');
 
