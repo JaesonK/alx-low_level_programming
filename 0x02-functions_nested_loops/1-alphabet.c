71094c01c7d478b5b3be35a8b2492f23224f09f1
Jamo <jameskouchade01@gmail.com>
2023-08-27 13:13:58 -0700

    alphet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * main - check the description
+ * Description: prints the alphabet, in lowercase, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+}
