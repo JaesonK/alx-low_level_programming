c75433802556907b6fa980cafc50293f1d09f63e
Jamo <jameskouchade01@gmail.com>
2023-08-28 14:41:11 -0700

    Digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Check Description
+ * @d: An integer input
+ * Description: This function prints the last digit of a number
+ * Return: last digit of number d
+ */
+int print_last_digit(int d)
+{
+	_putchar((d % 100 )+ '0');
+
+	return (0);
+}
