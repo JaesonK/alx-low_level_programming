38db3d39b58cd310329cbb29534cc3ca030e69cd
Jamo <jameskouchade01@gmail.com>
2023-08-28 11:58:01 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *_abs - Check Holberton
+ *_abs - Check Description
  * @n: An integre input
  * Description: This function returns absolute value of a number
- * Return: Absolut value of number r
+ * Return: Absolut value of number n
  */
 int _abs(int n)
 {
 	if (n < 0)
 		return (-1 * n);
 	else
-		return (r);
+		return (n);
 }
