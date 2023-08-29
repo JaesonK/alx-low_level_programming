ff2dfa1512e943d47e86184171a264e55f9e5b12
Jamo <jameskouchade01@gmail.com>
2023-08-29 12:06:25 -0700

    Naturel

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,11 +15,11 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf('%d', i);
+			printf("%d", i);
 			if (i != 98)
 			{
-				printf(',');
-				printf(',');
+				printf(",");
+				printf(" ");
 			}
 		}
 	}
@@ -27,11 +27,11 @@ void print_to_98(int n)
 	{
 		for (; i >= 98; i--)
 		{
-			printf('%d', i);
+			printf("%d", i);
 			if (i != 98)
 			{
-				printf(',');
-				printf(' ');
+				printf(",");
+				printf(" ");
 	}
 	_putchar('\n');
 }
