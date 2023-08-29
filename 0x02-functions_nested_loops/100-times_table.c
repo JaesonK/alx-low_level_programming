0e976c3d7b71bd9cb5c1475ee223f660a69f9b60
Jamo <jameskouchade01@gmail.com>
2023-08-29 14:21:20 -0700

    N times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int i, j, k;
 	
-	if (n > 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (i =  0; i <= n; i++)
 		{
@@ -27,6 +27,7 @@ void print_times_table(int n)
 						printf(", ");
 				}
 			}
+			print("\n");
 		}
 	}
 }
