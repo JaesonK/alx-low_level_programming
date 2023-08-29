6aeac06712e95b32be43b90e6dcc04a73abf129d
Jamo <jameskouchade01@gmail.com>
2023-08-29 02:56:48 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 			if (t < 9)
 			{
 				_putchar(t + '0');
-				if (j !=9)
+				if (j != 9)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -32,7 +32,6 @@ void times_table(void)
 				if (j != 9)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 				}
 				t = t + i;
