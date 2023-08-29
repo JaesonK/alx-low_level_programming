5f544920fe7cdb7b8388e8b2e12e34e6b80ad3ce
Jamo <jameskouchade01@gmail.com>
2023-08-29 14:14:20 -0700

    N times table

---
+++ 0x02-functions_nested_loops/100-print_times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table for numbers from 0-14
+ * @n: An input integer value
+ * Return: Nothing
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+	
+	if (n > 0 && n <= 15)
+	{
+		for (i =  0; i <= n; i++)
+		{
+			for (j = 0; j <=n; j++)
+			{
+				k = i*j;
+				printf("%d", k);
+				if (j != n)
+				{
+					if (k <= 9)
+						printf(",   ");
+					else if (k > 9 && k <= 99)
+						printf(",  ");
+					else
+						printf(", ");
+				}
+			}
+		}
+	}
+}
